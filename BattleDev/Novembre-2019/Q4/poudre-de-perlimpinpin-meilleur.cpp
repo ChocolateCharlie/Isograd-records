5f92b219b60c9202622c8f5c1615cefc2a613f56
ChocolateCharlie <aurore.amrit@gmail.com>
2019-11-29 16:40:08 +0100

    BattleDev Novembre 2019 - improve Q4 solution

--- BattleDev/Novembre-2019/Q4/poudre-de-perlimpinpin-meilleur.cpp
+++ BattleDev/Novembre-2019/Q4/poudre-de-perlimpinpin-meilleur.cpp
@@ -1,3 +1,6 @@
+// Cette solution peut encore être refusée, mais elle est meilleure.
+// --> choisir la poudre ne devrait pas se faire au détriment d'une pierre
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -5,45 +8,39 @@
 
 ContestExerciseImpl::ContestExerciseImpl() : Exercise() {}
 struct  Product {
-    double  v, q;
-    bool    stone = false;
-    int     stone_val;
+    int     va, p;  // va: valeur absolue (pour les pierres)
+    double  vr;     // vr: valeur relative (valeur par gramme)
+    bool    s = false;
 };
 
 void ContestExerciseImpl::main() {
-   int  N, M, C;
-   cin >> N >> M >> C;
+    int N, M, C;
+    cin >> N >> M >> C;
 
-   vector<Product>  p(N + M);
-   for (int i = 0; i < N; i++) {
-       double   a, b;
-       cin >> a >> b;
-       p[i].v = a / b;
-       p[i].q = b;
-       p[i].stone = true;
-       p[i].stone_val = a;
-   }
-   for (int i = 0; i < M; i++)  cin >> p[i + N].v >> p[i + N].q;
-   sort(p.begin(), p.end(), [](const Product &i, const Product &j) -> bool { return(i.v < j.v); });
+    vector<Product> p(N + M);
+    for (int i = 0; i < N; i++) {
+        cin >> p[i].va >> p[i].p;
+        p[i].vr = (double) p[i].va / (double) p[i].p;
+        p[i].s = true;
+    }
+    for (int i = 0; i < M; i++) cin >> p[i + N].vr >> p[i + N].p;
+    sort(p.begin(), p.end(), [](const Product &i, const Product &j) -> bool { return(i.vr < j.vr); });
 
     int ans;
     ans = 0;
 
-   while (C > 0 && !p.empty()) {
-       if (p.back().q > C) {
-           if (!p.back().stone) {
-               ans = ans + (C * p.back().v);
-               C = 0;
-           }
-       }
-       else {
-           if (p.back().stone)  ans += p.back().stone_val;
-           else                 ans = ans + (p.back().q * p.back().v);
-           C -= p.back().q;
-       }
+    while (C > 0 && !p.empty()) {
+        if (!p.back().s && p.back().p > C) {
+            ans += p.back().vr * C;
+            C = 0;
+        }
+        else if (p.back().p <= C) {
+            ans = (p.back().s ? ans + p.back().va : ans + p.back().vr * p.back().p);
+            C -= p.back().p;
+        }
 
-       p.pop_back();
-   }
+        p.pop_back();
+    }
 
-   cout << ans;
+    cout << ans;
 }
